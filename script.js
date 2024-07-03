fb4fef2641697b828f9c03655673ace02d49f091
Andy Le <4ndyle@gmail.com>
2024-07-02 22:03:28 -0700

    Create color change effect on individual squares on click

--- script.js
+++ script.js
@@ -3,22 +3,18 @@ const sketchContainer = document.querySelector(".sketch-container");
 const width = sketchContainer.offsetWidth;
 const height = sketchContainer.offsetHeight;
 
-console.log(width);
-console.log(height);
-
-
 // create first column 
 let column = document.createElement("div");
-let squaresTotal = 16; // user promted 
+let squaresTotal = 4; // user promted 
 
 let squareWidthHeight = width / squaresTotal;
-console.log(squareWidthHeight);
 
 for(let i = 0; i < squaresTotal; i++) {
   let square = document.createElement("div");
   square.style.width = `${squareWidthHeight}px`;
   square.style.height = `${squareWidthHeight}px`;
   square.classList.add("square");
+
   column.appendChild(square);
 }
 
@@ -27,3 +23,19 @@ for(let i = 0; i < squaresTotal; i++) {
   let cloneColumn = column.cloneNode(true);
   sketchContainer.appendChild(cloneColumn);
 }
+
+const pixel = document.querySelectorAll(".square");
+
+pixel.forEach(square => {
+  square.addEventListener("mouseover", () => {
+    console.log("over");
+  });
+  square.addEventListener("mousedown", () => {
+    console.log("down");
+    square.style.backgroundColor = "yellow";
+  });
+  square.addEventListener("mouseup", () => {
+    console.log("up");
+  });
+});
+
